1ab5c0578d98c4c6f4303fa58c15154e93580ab3
Varsha Kasar <44994924+varshakasar@users.noreply.github.com>
2019-11-12 22:57:55 +0530

    Update noteSchema.js

--- models/noteSchema.js
+++ models/noteSchema.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 let note = new mongoose.Schema({
-	subject:String,
-  content: String,
-  tag:String,
-  user:{
-	  type: mongoose.Schema.Types.ObjectId,
-	  ref:'user'
-       }
+	  subject:String,
+	  content: String,
+	  tag:String,
+	  user:{
+		  type: mongoose.Schema.Types.ObjectId,
+		  ref:'user'
+	       }
 });
 
 module.exports = note;
